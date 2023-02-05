86e8b8b2b7bf938bdd9e59ce8ec858df8c49af45
JinHyeong Park <pjhyng0125@naver.com>
2023-02-05 18:57:15 +0900

    [순수 JS 실습] 검색폼 만들기

---
+++ lecture-react/1-vanilla/js/Controller.js
@@ -0,0 +1,25 @@
+const tag = "[Controller]";
+
+export default class Controller {
+  constructor(store, { searchFormView }) {
+    this.store = store;
+
+    this.searchFormView = searchFormView;
+
+    this.subscribeViewEvents();
+  }
+
+  subscribeViewEvents() {
+    this.searchFormView.on("@submit", event =>
+      this.search(event.detail.value)
+    ).on("@reset", () => this.reset());
+  }
+
+  search(keyword) {
+    console.log(tag, keyword);
+  }
+
+  reset() {
+    console.log(tag, 'reset');
+  }
+}
---
+++ lecture-react/1-vanilla/js/main.js
@@ -0,0 +1,16 @@
+import Controller from "./Controller.js";
+import Store from "./store.js";
+import storage from "./storage.js";
+import SearchFormView from "./views/SearchFormView.js";
+
+document.addEventListener("DOMContentLoaded", main);
+
+function main() {
+  const store = new Store(storage);
+
+  const views = {
+    searchFormView: new SearchFormView
+  };
+
+  new Controller(store, views);
+}
