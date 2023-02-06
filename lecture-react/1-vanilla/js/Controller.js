aa04c49fcb2b08dace5255d86a951cc23c291be6
JinHyeong Park <pjhyng0125@naver.com>
2023-02-07 00:32:37 +0900

    [순수 JS 실습] 검색결과 view 생성

--- lecture-react/1-vanilla/js/Controller.js
+++ lecture-react/1-vanilla/js/Controller.js
@@ -1,25 +1,39 @@
 const tag = "[Controller]";
 
 export default class Controller {
-  constructor(store, { searchFormView }) {
+  constructor(store, { searchFormView, searchResultView }) {
+    console.log(tag, "constructor");
+
     this.store = store;
 
     this.searchFormView = searchFormView;
+    this.searchResultView = searchResultView;
 
     this.subscribeViewEvents();
   }
 
   subscribeViewEvents() {
-    this.searchFormView.on("@submit", event =>
-      this.search(event.detail.value)
-    ).on("@reset", () => this.reset());
+    this.searchFormView
+      .on("@submit", event => this.search(event.detail.value))
+      .on("@reset", () => this.reset());
   }
 
-  search(keyword) {
-    console.log(tag, keyword);
+  search(searchKeyword) {
+    console.log(tag, "search", searchKeyword);
+    this.store.search(searchKeyword);
+    this.render();
   }
 
   reset() {
-    console.log(tag, 'reset');
+    console.log(tag, "reset");
+  }
+
+  render() {
+    if (this.store.searchKeyword.length > 0) {
+      this.searchResultView.show(this.store.searchResult);
+      return;
+    }
+
+    this.searchResultView.hide();
   }
 }
