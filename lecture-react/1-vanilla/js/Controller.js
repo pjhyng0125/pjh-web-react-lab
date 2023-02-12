c822d13c8827bc6ab9ecdabb80ac42c066eaef3f
JinHyeong Park <pjhyng0125@naver.com>
2023-02-12 17:14:10 +0900

    [순수 JS] 추천/최근 검색어 기능 구현

--- lecture-react/1-vanilla/js/Controller.js
+++ lecture-react/1-vanilla/js/Controller.js
@@ -1,7 +1,18 @@
+import { TabType } from "./views/TabView.js";
+
 const tag = "[Controller]";
 
 export default class Controller {
-  constructor(store, { searchFormView, searchResultView, tabView }) {
+  constructor(
+    store,
+    {
+      searchFormView,
+      searchResultView,
+      tabView,
+      keywordListView,
+      historyListView,
+    }
+  ) {
     console.log(tag, "constructor");
 
     this.store = store;
@@ -9,6 +20,8 @@ export default class Controller {
     this.searchFormView = searchFormView;
     this.searchResultView = searchResultView;
     this.tabView = tabView;
+    this.keywordListView = keywordListView;
+    this.historyListView = historyListView;
 
     this.subscribeViewEvents();
     this.render();
@@ -17,11 +30,23 @@ export default class Controller {
   subscribeViewEvents() {
     // custom event 수신
     this.searchFormView
-      .on("@submit", event => this.search(event.detail.value))
+      .on("@submit", (event) => this.search(event.detail.value))
       .on("@reset", () => this.reset());
 
-      // this.tabView.on('@change', event => console.log(event));
-      this.tabView.on('@change', event => this.changeTab(event.detail.value))
+    // this.tabView.on('@change', event => console.log(event));
+    this.tabView.on("@change", (event) => this.changeTab(event.detail.value));
+
+    this.keywordListView.on("@click", (event) => {
+      const searchKeyword = event.detail.value;
+      this.search(searchKeyword);
+    });
+
+    this.historyListView
+      .on("@click", (event) => {
+        const searchKeyword = event.detail.value;
+        this.search(searchKeyword);
+      })
+      .on("@remove", (event) => this.removeHistory(event.detail.value));
   }
 
   search(searchKeyword) {
@@ -34,7 +59,7 @@ export default class Controller {
     console.log(tag, "reset");
 
     // X 버튼 클릭 시, 검색결과 초기화
-    this.store.searchKeyword = '';
+    this.store.searchKeyword = "";
     this.store.searchResult = [];
     this.render();
   }
@@ -45,17 +70,36 @@ export default class Controller {
     this.render();
   }
 
+  removeHistory(keyword) {
+    this.store.removeHistory(keyword);
+    this.render();
+  }
+
   render() {
     if (this.store.searchKeyword.length > 0) {
       return this.renderSearchResult();
     }
-    
+
     this.tabView.show(this.store.selectedTab);
+    if (this.store.selectedTab === TabType.KEYWORD) {
+      this.keywordListView.show(this.store.getKeywordList());
+      this.historyListView.hide();
+    } else if (this.store.selectedTab === TabType.HISTORY) {
+      this.keywordListView.hide();
+      this.historyListView.show(this.store.getHistoryList());
+    } else {
+      throw "사용할 수 없는 tab 입니다.";
+    }
+
     this.searchResultView.hide();
   }
 
   renderSearchResult() {
+    this.searchFormView.show(this.store.searchKeyword);
     this.tabView.hide();
+    this.keywordListView.hide();
+    this.historyListView.hide();
+
     this.searchResultView.show(this.store.searchResult);
   }
 }
--- lecture-react/1-vanilla/js/views/SearchFormView.js
+++ lecture-react/1-vanilla/js/views/SearchFormView.js
@@ -4,52 +4,59 @@ import View from "./View.js";
 const tag = "[SearchFormView]";
 
 export default class SearchFormView extends View {
-    constructor() {
-        console.log(tag, "constructor");
+  constructor() {
+    console.log(tag, "constructor");
 
-        // id search-form-view 인 element 찾아 this 할당
-        super(qs("#search-form-view"));
+    // id search-form-view 인 element 찾아 this 할당
+    super(qs("#search-form-view"));
 
-        this.inputElement = qs("[type=text]", this.element);
-        this.resetElement = qs("[type=reset]", this.element);
+    this.inputElement = qs("[type=text]", this.element);
+    this.resetElement = qs("[type=reset]", this.element);
 
-        this.showResetButton(false);
-        this.bindEvents();
+    this.showResetButton(false);
+    this.bindEvents();
+  }
+
+  showResetButton(visible = true) {
+    this.resetElement.style.display = visible ? "block" : "none";
+  }
+
+  bindEvents() {
+    on(this.inputElement, "keyup", () => this.handleKeyup());
+    this.on("submit", (event) => this.handleSubmit(event));
+
+    // x 버튼 클릭
+    on(this.resetElement, "click", () => this.handleReset());
+  }
+
+  handleKeyup() {
+    const { value } = this.inputElement;
+    this.showResetButton(value.length > 0);
+
+    // 검색어 삭제 : 입력 검색어 길이 비교
+    if (value.length <= 0) {
+      this.handleReset();
     }
+  }
 
-    showResetButton(visible = true) {
-        this.resetElement.style.display = visible ? "block" : "none";
-    }
+  handleSubmit(event) {
+    event.preventDefault(); // browser 기본 동작 방지
+    console.log(tag, "handleSubmit");
+    const { value } = this.inputElement;
 
-    bindEvents() {
-        on(this.inputElement, "keyup", () => this.handleKeyup());
-        this.on("submit", (event) => this.handleSubmit(event));
+    // 이벤트 emit
+    this.emit("@submit", { value });
+  }
 
-        // x 버튼 클릭
-        on(this.resetElement, "click", () => this.handleReset());
-    }
+  handleReset() {
+    console.log(tag, "handleReset");
+    this.emit("@reset", true);
+  }
 
-    handleKeyup() {
-        const { value } = this.inputElement;
-        this.showResetButton(value.length > 0);
+  show(searchKeyword = "") {
+    this.inputElement.value = searchKeyword;
+    this.showResetButton(this.inputElement.value.length > 0);
 
-        // 검색어 삭제 : 입력 검색어 길이 비교
-        if (value.length <= 0) {
-            this.handleReset();
-        }
-    }
-
-    handleSubmit(event) {
-        event.preventDefault(); // browser 기본 동작 방지
-        console.log(tag, "handleSubmit");
-        const { value } = this.inputElement;
-
-        // 이벤트 emit
-        this.emit("@submit", { value });
-    }
-
-    handleReset() {
-        console.log(tag, "handleReset");
-        this.emit("@reset", true);
-    }
+    super.show();
+  }
 }
